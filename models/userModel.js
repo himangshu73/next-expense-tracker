0816ea3f83e45924ad6ac80bb1aa98bf6af75246
himangshu <himangshu73@gmail.com>
2024-06-25 21:12:26 +0600

    created schema for users

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    unique: true,
+  },
+});
