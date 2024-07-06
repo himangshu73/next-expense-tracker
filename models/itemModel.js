4b424ae68e9806bab94e64b7f7fee59c0ddd7e9f
himangshu <himangshu73@gmail.com>
2024-07-06 17:40:30 +0600

    Item model added

---
+++ models/itemModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+  item: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  unit: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+export default mongoose.models.Item || mongoose.model("Item", itemSchema);
