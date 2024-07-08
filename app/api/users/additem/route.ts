57d91ccac0c4a95504e5f1588cbe4b725e9e1413
himangshu73 <himangshu73@gmail.com>
2024-07-08 08:44:22 +0600

    additem api

--- app/api/users/additem/route.ts
+++ app/api/users/additem/route.ts
@@ -1,18 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/app/dbConfig/dbConfig";
 import Item from "@/models/itemModel";
+import jwt from "jsonwebtoken";
+import { parse } from "cookie";
+
+interface JwtPayload {
+  id: string;
+  username: string;
+  email: string;
+}
 
 export async function POST(req: NextRequest) {
   await connect();
   try {
-    const { item, quantity, unit, price, user } = await req.json();
+    const cookies = parse(req.headers.get("cookie") || "");
+    const token = cookies.token;
+
+    if (!token) {
+      return NextResponse.json(
+        { messge: "Not authenticated" },
+        { status: 401 }
+      );
+    }
+
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET!) as JwtPayload;
+    const userId = decoded.id;
+
+    const { item, quantity, unit, price } = await req.json();
 
     const newItem = new Item({
       item,
       quantity,
       unit,
       price,
-      user,
+      user: userId,
     });
     await newItem.save();
     return NextResponse.json(
