7be7e8e211b88c71fe6f5d1597c3a02276b05879
himangshu <himangshu73@gmail.com>
2024-07-13 12:36:23 +0600

    expense card adjust

--- app/(components)/ExpenseCard.tsx
+++ app/(components)/ExpenseCard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-interface Item {
+export interface Item {
   _id: string;
   item: string;
   quantity: number;
@@ -9,46 +9,24 @@ interface Item {
   user: string;
 }
 
-const ExpenseCard = () => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [loading, setLoading] = useState(true);
+interface ExpenseCardProps {
+  item: Item;
+}
 
-  useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const response = await fetch("/api/items");
-        if (!response.ok) {
-          throw new Error("Error Fetching Items");
-        }
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchItems();
-  }, []);
-
-  if (loading) {
-    return <div>Loading....</div>;
-  }
+const ExpenseCard: React.FC<ExpenseCardProps> = ({ item }) => {
   return (
     <div className="bg-green-300 rounded-lg mb-4  p-4 shadow-md shadow-gray-500">
       <ul>
-        {items.map((item) => (
-          <li key={item._id}>
-            <div className="font-bold text-gray-700">Item: {item.item}</div>
-            <div className="font-bold text-gray-700">
-              Quantity: {item.quantity}
-              {item.unit}
-            </div>
-            <div className="font-bold text-gray-700">
-              Price: {item.price} Taka
-            </div>
-          </li>
-        ))}
+        <li>
+          <div className="font-bold text-gray-700">Item: {item.item}</div>
+          <div className="font-bold text-gray-700">
+            Quantity: {item.quantity}
+            {item.unit}
+          </div>
+          <div className="font-bold text-gray-700">
+            Price: {item.price} Taka
+          </div>
+        </li>
       </ul>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 "use client";
 
-import ExpenseCard from "./(components)/ExpenseCard";
+import { useEffect, useState } from "react";
+import ExpenseCard, { Item } from "./(components)/ExpenseCard";
 import ExpenseEntryForm from "./(components)/ExpenseEntryForm";
 import MonthlyExpense from "./(components)/MonthlyExpense";
 import { useAuth } from "./utils/AuthContext";
 
+const getItems = async () => {
+  try {
+    const res = await fetch("/api/items", {
+      cache: "no-store",
+    });
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get items", error);
+    return [];
+  }
+};
+
 export default function Home() {
+  const [items, setItems] = useState<Item[]>([]);
   const { loggedIn } = useAuth();
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const items = await getItems();
+      setItems(items);
+    };
+    fetchItems();
+  }, []);
   return (
     <div className="flex flex-wrap md:flex-nowrap">
       <div className="w-full md:w-1/2 flex flex-col p-4">
@@ -15,7 +37,9 @@ export default function Home() {
         <MonthlyExpense />
       </div>
       <div className="w-full md:w-1/2 flex flex-col p-4 justify-between">
-        <ExpenseCard />
+        {items.map((item) => (
+          <ExpenseCard key={item._id} item={item} />
+        ))}
       </div>
     </div>
   );
